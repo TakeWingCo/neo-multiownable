635b2a6fe729b50b3aa9419dc74d8c228a88068e
Lavrentiev Ivan <s02140218@stud.cs.msu.ru>
2018-06-24 18:56:20 +0300

    Add base functional of Multiownable

--- TakeWing.Neo.Multiownable/Multiownable.cs
+++ TakeWing.Neo.Multiownable/Multiownable.cs
@@ -8,8 +8,92 @@ using System.Numerics;
 
 namespace TakeWing.Neo.Multiownable
 {
-    public class Multiownable
+	/// <summary>
+	/// Library for easy multisig and consensus.
+	/// </summary>
+    public static class Multiownable
     {
-		
-    }
+		/// <summary>
+		/// Get number of owners.
+		/// </summary>
+		/// <returns>Number of owners</returns>
+		public static Byte GetNumberOfOwners()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get owner by his index.
+		/// </summary>
+		/// <param name="index">Index of owner</param>
+		/// <returns>Public key of owner</returns>
+		public static Byte[] GetOwnerByIndex(Byte index)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get index of owner.
+		/// </summary>
+		/// <param name="owner">Owner's public key</param>
+		/// <returns>Owner's index</returns>
+		public static Byte GetIndexByOwner(Byte[] owner)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get current generation of owners number.
+		/// </summary>
+		/// <returns>Generation of owners number</returns>
+		public static UInt64 GetGenerationOfOwners()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get array of owners.
+		/// </summary>
+		/// <returns>Array of owners</returns>
+		public static Byte[][] GetAllOwners()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Check if public key in owners list.
+		/// </summary>
+		/// <param name="publicKey">Public key</param>
+		/// <returns>True if public key in owners list, false else</returns>
+		public static Boolean IsOwner(Byte[] publicKey)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Transfer ownership to new owners list.
+		/// </summary>
+		/// <param name="initiator">Initiator's public key</param>
+		/// <param name="newOwners">Array of public keys</param>
+		/// <returns>Return true after successful transfer, else false</returns>
+		public static Boolean TransferOwnership(Byte[] initiator, Byte[][] newOwners)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Check, that timeout doesn't expire and minimal required number of owners accepts call of function.
+		/// </summary>
+		/// <param name="initiator"></param>
+		/// <param name="functionSignature"></param>
+		/// <param name="ownersCount"></param>
+		/// <param name="timeout"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static Boolean IsAcceptedBySomeOwners(Byte[] initiator, String functionSignature, Byte ownersCount, UInt32 timeout,
+			params Object[] args)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
