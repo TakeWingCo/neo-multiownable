033de8f28cc8de276d0391da74a1927a37b931f7
Соловьев Павел <soloway2010@yandex.ru>
2019-01-07 17:10:20 +0300

    Added TransferOwnership tests

--- TakeWing.Neo.Multiownable.Tests/MultiownableTests.cs
+++ TakeWing.Neo.Multiownable.Tests/MultiownableTests.cs
@@ -1,10 +1,10 @@
 ﻿using System.Numerics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Neo.Debugger.Core.Models;
 using Neo.Debugger.Core.Utils;
 using Neo.Emulation.API;
 using Neo.Lux.Utils;
 using Neo.VM.Types;
+using Neo.Emulation;
 
 namespace TakeWing.Neo.Multiownable.Tests
 {
@@ -24,13 +24,28 @@ namespace TakeWing.Neo.Multiownable.Tests
             }
         }
 
-        private static DebugManager Debugger;
-        private static TestContext TestContextInstance;
-        private static System.String PublicKey;
+        private DebugManager Debugger;
+        private TestContext TestContextInstance;
+
+        private static System.String[] PublicKeys;
 
         private void OnLogMessage(System.String msg)
         {
             TestContext.WriteLine(msg);
+        }
+
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            PublicKeys = new System.String[5];
+            PublicKeys[0] = "0218c0828e52456243ef56e3df865a55f3c6f83e291ff689a5a841a1bac6ab646b";
+            PublicKeys[1] = "020e55e594e4aa0c7d2759327b45cb640a595978507f7a67ab1e401657efb452ce";
+            PublicKeys[2] = "027f13d9e6ce080d18587e505f70e2f0667c3a59fae80bedb9b4f29575821ff7b0";
+            PublicKeys[3] = "0383592150122b607448793522aa596f366623036540d8d1354914cf4057e19ad0";
+            PublicKeys[4] = "032fd6fa29d1504731f2292a37527f18a5046b2c627a954f2aaa2b359694613a59";
+
+            var keyPair = DebuggerUtils.GetKeyFromString("347bc2bd9eb7b9f41a217a26dc5a3d2a3c25ece1c8bff1d5a146aaf4156e3436");
+            Runtime.invokerKeys = keyPair;
         }
 
         [TestInitialize]
@@ -38,20 +53,17 @@ namespace TakeWing.Neo.Multiownable.Tests
         {
             Debugger = new DebugManager();
             Debugger.LoadContract(@"..\..\..\TakeWing.Neo.Multiownable.SmartContractsForTests\bin\Debug\TakeWing.Neo.Multiownable.SmartContractsForTests.avm");
-
-            PublicKey = "02b3622bf4017bdfe317c58aed5f4c753f206b7db896046fa7d774bbc4bf7f8dc2";
-            var keyPair = DebuggerUtils.GetKeyFromString("347bc2bd9eb7b9f41a217a26dc5a3d2a3c25ece1c8bff1d5a146aaf4156e3436");
-            Runtime.invokerKeys = keyPair;
+            Debugger.Emulator.checkWitnessMode = CheckWitnessMode.AlwaysTrue;
 
             Runtime.OnLogMessage = OnLogMessage;
         }
 
         [TestMethod]
-        public void TransferOwnershipTest()
+        public void SetOwnershipTest()
         {
             var operation = "TransferOwnership";
 
-            var args = System.String.Format("\"{0}\",[\"0x{1}\", 1, \"0x{1}\"]", operation, PublicKey);
+            var args = $"\"{operation}\",[\"0x{PublicKeys[0]}\", 5, \"0x{PublicKeys[0]}\", \"0x{PublicKeys[1]}\", \"0x{PublicKeys[2]}\", \"0x{PublicKeys[3]}\", \"0x{PublicKeys[4]}\"]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -64,32 +76,109 @@ namespace TakeWing.Neo.Multiownable.Tests
         }
 
         [TestMethod]
-        public void GetNumberOfOwnersTest()
+        public void TransferOwnershipTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
 
-            var operation = "GetNumberOfOwners";
+            var operation = "TransferOwnership";
 
-            var args = System.String.Format("\"{0}\",[]", operation);
+            // First vote
+            var args = $"\"{operation}\",[\"0x{PublicKeys[0]}\", 1, \"0x{PublicKeys[2]}\"]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
             Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
             Debugger.Emulator.Run();
 
+            // Second vote
+            args = $"\"{operation}\",[\"0x{PublicKeys[1]}\", 1, \"0x{PublicKeys[2]}\"]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            // Third vote
+            args = $"\"{operation}\",[\"0x{PublicKeys[2]}\", 1, \"0x{PublicKeys[2]}\"]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            // Check if correct number of owners
+            operation = "GetNumberOfOwners";
+
+            args = $"\"{operation}\",[]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
             var result = Debugger.Emulator.GetOutput();
 
             Assert.AreEqual(BigInteger.Parse("1"), result.GetBigInteger());
+
+            // Check if correct owner
+            operation = "IsOwner";
+
+            args = $"\"{operation}\",[\"0x{PublicKeys[2]}\"]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            result = Debugger.Emulator.GetOutput();
+
+            Assert.IsTrue(result.GetBoolean());
         }
 
         [TestMethod]
-        public void GetIndexByOwnerTest()
+        public void EnoughVotesCallTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
+
+            var operation = "Call";
+
+            // First vote
+            var args = $"\"{operation}\",[\"0x{PublicKeys[0]}\", \"Boolean TransferOwnership(Byte[], Byte[][])\", 3, 1000, 0]";
+            var inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            // Second vote
+            args = $"\"{operation}\",[\"0x{PublicKeys[1]}\", \"Boolean TransferOwnership(Byte[], Byte[][])\", 3, 1000, 0]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            //Third vote
+            args = $"\"{operation}\",[\"0x{PublicKeys[2]}\", \"Boolean TransferOwnership(Byte[], Byte[][])\", 3, 1000, 0]";
+            inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            var result = Debugger.Emulator.GetOutput();
+
+            Assert.IsTrue(result.GetBoolean());
+        }
+
+        [TestMethod]
+        public void GetCorrectIndexByOwnerTest()
+        {
+            SetOwnershipTest();
 
             var operation = "GetIndexByOwner";
 
-            var args = System.String.Format("\"{0}\",[\"0x{1}\"]", operation, PublicKey);
+            var args = $"\"{operation}\",[\"0x{PublicKeys[2]}\"]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -98,17 +187,36 @@ namespace TakeWing.Neo.Multiownable.Tests
 
             var result = Debugger.Emulator.GetOutput();
 
-            Assert.AreEqual(BigInteger.Parse("1"), result.GetBigInteger());
+            Assert.AreEqual(BigInteger.Parse("3"), result.GetBigInteger());
         }
 
         [TestMethod]
-        public void GetOwnerByIndexTest()
+        public void GetWrongIndexByOwnerTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
+
+            var operation = "GetIndexByOwner";
+
+            var args = $"\"{operation}\",[\"0xabcd\"]";
+            var inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            var result = Debugger.Emulator.GetOutput();
+
+            Assert.AreEqual("", result.GetString());
+        }
+
+        [TestMethod]
+        public void GetCorrectOwnerByIndexTest()
+        {
+            SetOwnershipTest();
 
             var operation = "GetOwnerByIndex";
 
-            var args = System.String.Format("\"{0}\",[1]", operation);
+            var args = $"\"{operation}\",[2]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -117,17 +225,17 @@ namespace TakeWing.Neo.Multiownable.Tests
 
             var result = Debugger.Emulator.GetOutput();
 
-            CollectionAssert.AreEqual(PublicKey.HexToBytes(), result.GetByteArray());
+            CollectionAssert.AreEqual(PublicKeys[1].HexToBytes(), result.GetByteArray());
         }
 
         [TestMethod]
-        public void GetGenerationOfOwnersTest()
+        public void GetWrongOwnerByIndexTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
 
-            var operation = "GetGenerationOfOwners";
+            var operation = "GetOwnerByIndex";
 
-            var args = System.String.Format("\"{0}\",[]", operation);
+            var args = $"\"{operation}\",[10]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -136,17 +244,17 @@ namespace TakeWing.Neo.Multiownable.Tests
 
             var result = Debugger.Emulator.GetOutput();
 
-            Assert.AreEqual(BigInteger.Parse("1"), result.GetBigInteger());
+            Assert.AreEqual("", result.GetString());
         }
 
         [TestMethod]
-        public void IsOwnerTest()
+        public void IsCorrectOwnerTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
 
             var operation = "IsOwner";
 
-            var args = System.String.Format("\"{0}\",[\"0x{1}\"]", operation, PublicKey);
+            var args = $"\"{operation}\",[\"0x{PublicKeys[3]}\"]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -159,30 +267,13 @@ namespace TakeWing.Neo.Multiownable.Tests
         }
 
         [TestMethod]
-        public void GetAllOwnersTest()
+        public void IsWrongOwnerTest()
         {
-            TransferOwnershipTest();
+            SetOwnershipTest();
 
-            var operation = "GetAllOwners";
+            var operation = "IsOwner";
 
-            var args = System.String.Format("\"{0}\",[]", operation);
-            var inputs = DebuggerUtils.GetArgsListAsNode(args);
-
-            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
-            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
-            Debugger.Emulator.Run();
-
-            var result = Debugger.Emulator.GetOutput() as Array;
-
-            CollectionAssert.AreEqual(PublicKey.HexToBytes(), result[0].GetByteArray());
-        }
-
-        [TestMethod]
-        public void UnknownOperationTest()
-        {
-            var operation = "Unknown";
-
-            var args = System.String.Format("\"{0}\",[]", operation);
+            var args = $"\"{operation}\",[\"0xabcd\"]";
             var inputs = DebuggerUtils.GetArgsListAsNode(args);
 
             var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
@@ -192,6 +283,64 @@ namespace TakeWing.Neo.Multiownable.Tests
             var result = Debugger.Emulator.GetOutput();
 
             Assert.IsFalse(result.GetBoolean());
+        }
+
+        [TestMethod]
+        public void GetAllOwnersTest()
+        {
+            SetOwnershipTest();
+
+            var operation = "GetAllOwners";
+
+            var args = $"\"{operation}\",[]";
+            var inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            var result = Debugger.Emulator.GetOutput() as Array;
+
+            for(var i = 0; i < 5; i++)
+                CollectionAssert.AreEqual(PublicKeys[i].HexToBytes(), result[i].GetByteArray());
+        }
+
+        [TestMethod]
+        public void GetGenerationOfOwnersTest()
+        {
+            SetOwnershipTest();
+
+            var operation = "GetGenerationOfOwners";
+
+            var args = $"\"{operation}\",[]";
+            var inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            var result = Debugger.Emulator.GetOutput();
+
+            Assert.AreEqual(BigInteger.Parse("1"), result.GetBigInteger());
+        }
+
+        [TestMethod]
+        public void GetNumberOfOwnersTest()
+        {
+            SetOwnershipTest();
+
+            var operation = "GetNumberOfOwners";
+
+            var args = $"\"{operation}\",[]";
+            var inputs = DebuggerUtils.GetArgsListAsNode(args);
+
+            var script = Debugger.Emulator.GenerateLoaderScriptFromInputs(inputs, Debugger.ABI);
+            Debugger.Emulator.Reset(script, Debugger.ABI, "Main");
+            Debugger.Emulator.Run();
+
+            var result = Debugger.Emulator.GetOutput();
+
+            Assert.AreEqual(BigInteger.Parse("5"), result.GetBigInteger());
         }
     }
 }
